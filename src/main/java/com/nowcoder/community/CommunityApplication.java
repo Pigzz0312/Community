c7ddf43c80a274feee12e62ff5df54c0bb3b4217
pigzz <pigzz0312@gmail.com>
2023-05-31 14:06:17 +0800

    第三次提交

--- src/main/java/com/nowcoder/community/CommunityApplication.java
+++ src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CommunityApplication {
 
     public static void main(String[] args) {
-        //1
+        //3
         SpringApplication.run(CommunityApplication.class, args);
     }
 
