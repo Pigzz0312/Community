c9c88cf282a2f7c6b51d120405a59c82ab9b8b2c
pigzz <pigzz0312@gmail.com>
2023-06-01 11:10:20 +0800

    第2次提交

--- src/test/java/com/nowcoder/community/CommunityApplicationTests.java
+++ src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -4,9 +4,12 @@ import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.MailClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import java.util.List;
 
@@ -18,6 +21,26 @@ class CommunityApplicationTests {
 
     @Autowired
     private DiscussPostMapper discussPortMapper;
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    void sentMailTest(){
+        mailClient.sendMail("1270304922@qq.com", "TEST", "test");
+    }
+
+    @Test
+    void sentHtmlMailTest(){
+        Context context = new Context();
+        context.setVariable("username", "zjh");
+
+        String content = templateEngine.process("/mail/demo", context);
+        mailClient.sendMail("1270304922@qq.com", "HTML", content);
+    }
 
     @Test
     void contextLoads() {
